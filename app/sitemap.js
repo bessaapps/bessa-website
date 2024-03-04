01a98580c963291412dd2bccb8c53cdb6250866a
stupendousweb <topher@stupendousweb.com>
2024-03-04 08:06:15 -0800

    Think twice, code once.

---
+++ app/sitemap.js
@@ -0,0 +1,52 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://bessssssa.com",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1
+    },
+    {
+      url: "https://bessssssa.com/cookies",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7
+    },
+    {
+      url: "https://bessssssa.com/donate",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7
+    },
+    {
+      url: "https://bessssssa.com/eula",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7
+    },
+    {
+      url: "https://bessssssa.com/lgbtqia-community",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7
+    },
+    {
+      url: "https://bessssssa.com/privacy",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7
+    },
+    {
+      url: "https://bessssssa.com/support",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7
+    },
+    {
+      url: "https://bessssssa.com/terms",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7
+    }
+  ];
+}
