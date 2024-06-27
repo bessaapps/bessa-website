1c782198ab2ea68d49b850e2ea58e850afdc1704
stupendousweb <topher@stupendousweb.com>
2024-06-27 15:46:07 -0700

    Think twice, code once.

--- app/page.js
+++ app/page.js
@@ -157,9 +157,11 @@ export default function Home() {
           </Heading>
         </Container>
         <Container maxW={"container.lg"} py={32}>
-          <SimpleGrid columns={[1, 3]} gap={4}>
+          <SimpleGrid columns={[1, 3]} gap={[4, 8]}>
             <GridItem>
-              <Image src={Mockup1} alt={"An LGBTQ Social Media App"} />
+              <Box mb={[4, 8]}>
+                <Image src={Mockup1} alt={"An LGBTQ Social Media App"} />
+              </Box>
               <Heading my={4}>Make some Noise!</Heading>
               <Text mb={4}>
                 Sharing your LGBTQ pride is now as fun as ever. By sharing
@@ -169,7 +171,9 @@ export default function Home() {
               </Text>
             </GridItem>
             <GridItem>
-              <Image src={Mockup2} alt={"An LGBTQ Social Media App"} />
+              <Box mb={[4, 8]}>
+                <Image src={Mockup2} alt={"An LGBTQ Social Media App"} />
+              </Box>
               <Heading my={4}>Socialize with Community</Heading>
               <Text mb={4}>
                 Networking is a huge part of building queer community. Find
@@ -179,7 +183,9 @@ export default function Home() {
               </Text>
             </GridItem>
             <GridItem>
-              <Image src={Mockup3} alt={"An LGBTQ Social Media App"} />
+              <Box mb={[4, 8]}>
+                <Image src={Mockup3} alt={"An LGBTQ Social Media App"} />
+              </Box>
               <Heading my={4}>Build your Following</Heading>
               <Text mb={4}>
                 Share your LGBTQ thoughts, ideas, and other good stuff through
