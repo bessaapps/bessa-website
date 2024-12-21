df4a22dc9f7ed04742cf5801e0f2f9e80cc4e255
stupendousweb <topher@stupendousweb.com>
2024-12-21 12:48:09 -0800

    Think twice, code once.

--- app/blog/[postSlug]/layout.js
+++ app/blog/[postSlug]/layout.js
@@ -10,43 +10,67 @@ import {
 import { appStores } from "@/utils/constants";
 import axios from "axios";
 import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+import dayjs from "dayjs";
 
 export async function generateMetadata({ params }) {
   return await axios
     .get("https://blog.getbessa.com/wp-json/wp/v2/posts", {
       params: {
         slug: params?.postSlug,
-        _embed: ["wp:term", "wp:featuredmedia", "author"]
+        _embed: ["wp:featuredmedia", "author"]
       }
     })
     .then((response) => {
       const post = response?.data?.[0];
-      //     title: `${post.title} | ${title}`,
-      //         openGraph: {
-      //     title: `${post.title} | ${title}`,
-      //         url: `${url}/blog/${post.category.slug}/${post.slug}`,
-      //         siteName: "Bessa",
-      //         images: [
-      //       {
-      //         url: `${url}/images/${post.slug}.png`,
-      //         width: 800,
-      //         height: 600
-      //       }
-      //     ],
-      //         locale: "en_US",
-      //         type: "website"
-      //   },
-      //     alternates: {
-      //       canonical: `${url}/blog/${post.category.slug}/${post.slug}`
-      //     }
-      //   };
-      //   setPost(response?.data?.[0])
-      // });
+
       return {
         title: `${post?.title?.rendered} | Bessa | An LGBTQ Social Media App`,
+        description: post?.excerpt?.rendered,
+        authors: [
+          {
+            name: post?.post?._embedded?.author?.[0]?.name,
+            url: `https://${process.env?.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}/authors/${post?.post?._embedded?.author?.[0]?.name?.toLowerCase()?.replace(" ", "-")}`
+          }
+        ],
+        languages: {
+          "en-US": "/en-US"
+        },
         openGraph: {
           title: `${post?.title?.rendered} | Bessa | An LGBTQ Social Media App`,
-          url: process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+          description: post?.excerpt?.rendered,
+          url: `https://${process.env?.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`,
+          siteName: "Bessa | An LGBTQ Social Media App",
+          images: [
+            {
+              url: post?._embedded?.["wp:featuredmedia"]?.[0]?.source_url,
+              width: 800,
+              height: 600
+            }
+          ],
+          locale: "en_US",
+          type: "article",
+          publishedTime: dayjs(post?.date)?.format(),
+          authors: [post?.post?._embedded?.author?.[0]?.name]
+        },
+        twitter: {
+          card: "summary_large_image",
+          title: `${post?.title?.rendered} | Bessa | An LGBTQ Social Media App`,
+          description: post?.excerpt?.rendered,
+          creator: "@getbessa",
+          images: {
+            url: post?._embedded?.["wp:featuredmedia"]?.[0]?.source_url,
+            alt: post?.title?.rendered
+          }
+        },
+        appLinks: {
+          ios: {
+            url: "https://apps.apple.com/us/app/bessa-lgbtq-social-media-app/id6471383138?platform=iphone",
+            app_store_id: 6471383138
+          },
+          android: {
+            package: "com.bessa.bessa",
+            app_name: "Bessa LGBTQ Social Media App"
+          }
         }
       };
     });
@@ -60,7 +84,7 @@ export default function PostLayout({ children }) {
         <Card>
           <Container maxW={"container.sm"} my={[4, 16]}>
             <Heading color={"gray.900"} textAlign={"center"} mb={4}>
-              Be with Friends!{process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}
+              Be with Friends!
             </Heading>
             <Text textAlign={"center"} mb={8}>
               Find LGBTQ members with shared interests near you. Make queer
