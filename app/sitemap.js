c6fab415981520f5311b4b74b06ea4e6d9878c75
stupendous-web <topher@stupendousweb.com>
2024-12-21 14:12:02 -0800

    Think twice. Code once.

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,4 +1,14 @@
-export default function sitemap() {
+import axios from "axios";
+
+export default async function sitemap() {
+  let posts = [];
+
+  await axios
+    .get("https://blog.getbessa.com/wp-json/wp/v2/posts")
+    .then((response) => {
+      posts = response?.data;
+    });
+
   return [
     {
       url: "https://getbessa.com",
@@ -6,47 +16,56 @@ export default function sitemap() {
       changeFrequency: "daily",
       priority: 1
     },
+    ...posts?.map((post) => {
+      return {
+        url: `https://getbessa.com/posts/${post?.slug}`,
+        lastModified: new Date(post?.modified),
+        changeFrequency: "daily",
+        priority: 0.8
+      };
+    }),
+
     {
       url: "https://getbessa.com/blog-content-writer",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.7
+      priority: 0.8
     },
     {
       url: "https://getbessa.com/support",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.7
+      priority: 0.8
     },
     {
       url: "https://getbessa.com/guidelines",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.7
+      priority: 0.8
     },
     {
       url: "https://getbessa.com/cookies",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.7
+      priority: 0.5
     },
     {
       url: "https://getbessa.com/eula",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.7
+      priority: 0.5
     },
     {
       url: "https://getbessa.com/privacy",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.7
+      priority: 0.5
     },
     {
       url: "https://getbessa.com/terms",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.7
+      priority: 0.5
     }
   ];
 }
