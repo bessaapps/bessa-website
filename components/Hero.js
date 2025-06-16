098e4a63f0349257bb9262a051b9a80dfa7e84e6
topher <topherjamesknoll@gmail.com>
2025-06-16 12:54:59 -0700

    Think twice. Code once.

--- components/Hero.js
+++ components/Hero.js
@@ -5,17 +5,18 @@ export default function Hero({ heading, lead, bgImage }) {
     <Box bgImage={bgImage} bgPosition={"center"} bgSize={"cover"}>
       <Center minH={"calc(100vh - 129px)"} p={4}>
         <Stack textAlign={"center"}>
-          <Box>
-            <Tag
-              as={"h1"}
-              colorScheme={"whiteAlpha"}
-              size={"lg"}
-              mb={4}
-              fontSize={"1rem !important"}
-            >
-              {lead}
-            </Tag>
-          </Box>
+          {lead && (
+            <Box>
+              <Tag
+                colorScheme={"whiteAlpha"}
+                size={"lg"}
+                mb={4}
+                fontSize={"1rem !important"}
+              >
+                {lead}
+              </Tag>
+            </Box>
+          )}
           <Heading as={"h1"} color={"white"} textAlign={"center"}>
             {heading}
           </Heading>
