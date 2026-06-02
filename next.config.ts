79c168130fcd0dab0b56216286b17611327af0d8
topher <topherjamesknoll@gmail.com>
2026-06-02 11:05:05 -0700

    Add new page for free drink coasters for LGBT bars; configure redirect from `/coasters` to improve discoverability.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  redirects: async () => [
+    {
+      source: "/coasters",
+      destination: "/free-drink-coasters-for-lgbt-bars",
+      permanent: true
+    }
+  ]
 };
 
 export default nextConfig;
